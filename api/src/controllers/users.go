d24fcbc2eab5f658904e6a31d2a5ed148b510283
Nathan Fabio <nathan_3_m@hotmail.com>
2023-02-25 21:46:00 -0300

    created package repositories

--- api/src/controllers/users.go
+++ api/src/controllers/users.go
@@ -1,10 +1,41 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/database"
+	"api/src/models"
+	"api/src/repositories"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
 //CreateUser creates a new user in the database.
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Creating User!"))
+	requestBody, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var user models.User
+	if err = json.Unmarshal(requestBody, &user); err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := database.Connection()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+
+	repository := repositories.NewUsersRepositories(db)
+	UserID, err := repository.Create(user)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.Write([]byte(fmt.Sprintf("ID entered: %d", UserID)))
 }
 
 //SearchUsers Searches for all users saved in the database.
---
+++ api/src/repositories/users.go
@@ -0,0 +1,38 @@
+package repositories
+
+import (
+	"api/src/models"
+	"database/sql"
+)
+
+type users struct {
+	db *sql.DB
+}
+
+//NewUsersRepositories creates a user repository
+func NewUsersRepositories(db *sql.DB) *users {
+	return &users{db}
+}
+
+
+//Create inserts a user into the database
+func (u users) Create(user models.User) (uint32, error) {
+	statment, err := u.db.Prepare("insert into users (name, nick, email, password) values(?, ?, ?, ?)",)
+	if err != nil {
+		return 0, err
+	}
+	defer statment.Close()
+
+	result, err := statment.Exec(user.Name, user.Nick, user.Email, user.Password)
+	if err != nil {
+		return 0, err
+	}
+
+
+	lastEnteredID, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return uint32(lastEnteredID), nil
+}
